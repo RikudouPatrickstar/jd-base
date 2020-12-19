c84b0e8b9ccb4b582f57d747fc1cafa42bd1f768
EvineDeng <evinedeng@foxmail.com>
2020-12-19 22:22:27 +0800

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 [ ! -d ${JD_DIR}/log ] && mkdir -p ${JD_DIR}/log
 crond
 
-if [ -f ${JD_DIR}/crontab.list ]
+if [ -f ${JD_DIR}/config/crontab.list ]
 then
-  crontab ${JD_DIR}/crontab.list
+  crontab ${JD_DIR}/config/crontab.list
 else
-  cp -f ${JD_DIR}/sample/docker.list.sample ${JD_DIR}/crontab.list
-  crontab ${JD_DIR}/crontab.list
+  cp -f ${JD_DIR}/sample/docker.list.sample ${JD_DIR}/config/crontab.list
+  crontab ${JD_DIR}/config/crontab.list
 fi
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
