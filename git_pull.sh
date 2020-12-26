3b84c20bd8daa53b55882785710317fc9c4bb5c1
EvineDeng <evinedeng@foxmail.com>
2020-12-26 14:49:27 +0800

    尽量兼容MacOS，实在不行建议MacOS安装GNU版grep，抛弃BSD版grep

--- git_pull.sh
+++ git_pull.sh
@@ -2,8 +2,8 @@
 
 ## Author: Evine Deng
 ## Source: https://github.com/EvineDeng/jd-base
-## Modified： 2020-12-25
-## Version： v3.3.8
+## Modified： 2020-12-26
+## Version： v3.3.9
 
 ## 文件路径、脚本网址、文件版本以及各种环境的判断
 if [ -f /proc/1/cgroup ]
@@ -149,9 +149,9 @@ function Diff_Cron {
   if [ -f ${ListCron} ]; then
     if [ -n "${isDocker}" ]
     then
-      grep -E " j[dr]_\w+" ${ListCron} | perl -pe "s|.+ (j[dr]_\w+).*|\1|" | uniq > ${ListTask}
+      grep -E " j[dr]_\w+" ${ListCron} | perl -pe "s|.+ (j[dr]_\w+).*|\1|" | uniq | sort > ${ListTask}
     else
-      grep "${ShellDir}/" ${ListCron} | grep -E " j[dr]_\w+" | perl -pe "s|.+ (j[dr]_\w+).*|\1|" | uniq > ${ListTask}
+      grep "${ShellDir}/" ${ListCron} | grep -E " j[dr]_\w+" | perl -pe "s|.+ (j[dr]_\w+).*|\1|" | uniq | sort > ${ListTask}
     fi
     grep -E "j[dr]_\w+\.js" ${ScriptsDir}/docker/crontab_list.sh | perl -pe "s|.+(j[dr]_\w+)\.js.+|\1|" | sort > ${ListJs}
     grep -vwf ${ListTask} ${ListJs} > ${ListJsAdd}
