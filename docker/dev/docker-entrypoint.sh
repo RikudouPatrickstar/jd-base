85042e408ebc5246e17ee25c3c9cf9f4d78a79ef
EvineDeng <evinedeng@foxmail.com>
2021-01-08 17:29:12 +0800

    Fix

--- docker/dev/docker-entrypoint.sh
+++ docker/dev/docker-entrypoint.sh
@@ -81,7 +81,7 @@ fi
 
 echo -e "========================4. 启动控制面板========================\n"
 if [[ ${ENABLE_WEB_PANEL} == true ]]; then
-  pm2 start ${JD_DIR}/panelserver.js
+  pm2 start ${JD_DIR}/panel/server.js
   echo -e "控制面板启动成功...\n"
   echo -e "如未修改用户名密码，则初始用户名为：admin，初始密码为：adminadmin\n"
   echo -e "请访问 http://<ip>:5678 登陆并修改配置...\n"
