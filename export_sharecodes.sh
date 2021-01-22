475bcfbbfa592bdc6c3b56837ac57d10cda598bd
EvineDeng <evinedeng@foxmail.com>
2021-01-22 15:47:57 +0800

    Optimize code

--- export_sharecodes.sh
+++ export_sharecodes.sh
@@ -18,35 +18,17 @@ function Cat_Scodes {
   if [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
     cd ${LogDir}/jd_$1
     for log in $(ls -r); do
-      codes=$(grep -${Opt} $2 ${log} | perl -pe "s| ||")
-      [[ ${codes} ]] && break
-    done
-    [[ ${codes} ]] && echo ${codes} || echo "从日志中未找到任何互助码..."
-  else
-    echo "还没有产生日志..."
-  fi
-}
-
-## 导出口袋书店互助码，没法用通用程序
-function Cat_ScodesBookShop {
-  if [ -d ${LogDir}/jd_bookshop ] && [[ $(ls ${LogDir}/jd_bookshop) != "" ]]; then
-    cd ${LogDir}/jd_bookshop
-    for log in $(ls -r); do
-      codes=$(perl -pe "s|信息获取成功\n||g" ${log} | grep -${Opt} "您的好友助力码为" | perl -pe "{s|您的好友助力码为||g; s|用户||g}")
-      [[ ${codes} ]] && break
-    done
-    [[ ${codes} ]] && echo ${codes} || echo "从日志中未找到任何互助码..."
-  else
-    echo "还没有产生日志..."
-  fi
-}
-
-## 导出签到领现金互助码，没法用通用程序
-function Cat_ScodesCash {
-  if [ -d ${LogDir}/jd_cash ] && [[ $(ls ${LogDir}/jd_cash) != "" ]]; then
-    cd ${LogDir}/jd_cash
-    for log in $(ls -r); do
-      codes=$(perl -0777 -pe "s|\*+\n+||g" ${log} | grep -${Opt} "您的助力码为" | perl -pe "{s|\*+开始||g; s|您的助力码为|：|g}")
+      case $1 in
+        bookshop)
+          codes=$(perl -pe "s|信息获取成功\n||g" ${log} | grep -${Opt} "您的好友助力码为" | perl -pe "{s|您的好友助力码为||g; s|用户||g}")
+          ;;
+        cash)
+          codes=$(perl -0777 -pe "s|\*+\n+||g" ${log} | grep -${Opt} "您的助力码为" | perl -pe "{s|\*+开始||g; s|您的助力码为|：|g}")
+          ;;
+        *)
+          codes=$(grep -${Opt} $2 ${log} | perl -pe "s| ||")
+          ;;
+      esac
       [[ ${codes} ]] && break
     done
     [[ ${codes} ]] && echo ${codes} || echo "从日志中未找到任何互助码..."
@@ -62,15 +44,12 @@ function Cat_All {
     echo -e "\n${Name2[i]}："
     Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码" | perl -pe "s|的${Name2[i]}好友互助码||g"
   done
-  echo -e "\n口袋书店：" && Cat_ScodesBookShop
-  echo -e "\n签到领现金：" && Cat_ScodesCash
 }
 
 ## 执行并写入日志
 LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
 LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
-Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc)
-Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场)
+Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash)
+Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金)
 [ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
-touch ${LogFile}
 Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|; s| |\n|g}" | tee ${LogFile}
