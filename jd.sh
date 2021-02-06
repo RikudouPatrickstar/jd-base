b201b3f73ec60a4d3c57e72f7c0df409389c3479
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2021-02-06 15:10:22 +0800

    移除失效活动

--- jd.sh
+++ jd.sh
@@ -72,11 +72,7 @@ function Combin_All {
   export JXNCTOKENS=$(Combin_Sub TokenJxnc)
   export BOOKSHOP_SHARECODES=$(Combin_Sub ForOtherBookShop)
   export JD_CASH_SHARECODES=$(Combin_Sub ForOtherCash)
-  export JDNIAN_SHARECODES=$(Combin_Sub ForOtherNian)
-  export JDNIANPK_SHARECODES=$(Combin_Sub ForOtherNianPk)
   export JDSGMH_SHARECODES=$(Combin_Sub ForOtherSgmh)
-  export JSMOBILEFESTIVAL_SHARECODES=$(Combin_Sub ForOtherJdMobileFestival)
-  export JD818_SHARECODES=$(Combin_Sub ForOtherJd818)
   export JDNY_SHARECODES=$(Combin_Sub ForOtherNY)
 }
 
