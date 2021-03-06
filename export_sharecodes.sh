4cb9191a41b0d6ec9dd94b961fbe1d8a32cdaaaf
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2021-03-06 12:42:12 +0800

    调整互助码填写区域，新增导出互助码模板

--- export_sharecodes.sh
+++ export_sharecodes.sh
@@ -3,60 +3,101 @@
 ## 路径、环境判断
 ShellDir=$(cd "$(dirname "$0")";pwd)
 LogDir=${ShellDir}/log
+ConfigDir=${ShellDir}/config
+FileConf=${ConfigDir}/config.sh
 Tips="从日志中未找到任何互助码"
 
-## 所有有互助码的活动，只需要把脚本名称去掉前缀 jd_ 后列在 Name1 中，将其中文名称列在 Name2 中，对应config.sh 中互助码后缀列在 Name3 中即可。
+## 所有有互助码的活动，只需要把脚本名称去掉前缀 jd_ 后列在 Name1 中，将其中文名称列在 Name2 中，对应 config.sh 中互助码后缀列在 Name3 中即可。
 ## Name1、 Name2 和 Name3 中的三个名称必须一一对应。
 Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash sgmh cfd)
 Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金 闪购盲盒 京喜财富岛)
 Name3=(Fruit Pet Bean DreamFactory JdFactory Joy Jdzz Jxnc BookShop Cash Sgmh Cfd)
 
-## 获取Cookie个数
-CookieNum=$(grep -E "Cookie[0-9]{1,}=" ${ShellDir}/config/config.sh | wc -l)
+## 导入 config.sh
+function Import_Conf {
+  if [ -f ${FileConf} ]
+  then
+    . ${FileConf}
+    if [ -z "${Cookie1}" ]; then
+      echo -e "请先在 config.sh 中配置好 Cookie\n"
+      exit 1
+    fi
+  else
+    echo -e "配置文件 ${FileConf} 不存在，请先按教程配置好该文件\n"
+    exit 1
+  fi
+}
+
+## 用户数量 UserSum
+function Count_UserSum {
+  for ((i=1; i<=1000; i++)); do
+    Tmp=Cookie$i
+    CookieTmp=${!Tmp}
+    [[ ${CookieTmp} ]] && UserSum=$i || break
+  done
+}
 
 ## 导出互助码的通用程序
 function Cat_Scodes {
-  if [ -d ${LogDir}/jd_$2 ] && [[ $(ls ${LogDir}/jd_$2) != "" ]]; then
-    cd ${LogDir}/jd_$2
+  if [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
+    cd ${LogDir}/jd_$1
     ## 导出Cookie列表助力码变量
     for log in $(ls -r); do
       case $# in
-        3)
-          codes=$(cat ${log} | grep -E "开始【京东账号|您的(好友)?助力码为" | uniq | perl -0777 -pe "{s|\*||g; s|开始||g; s|\n您的(好友)?助力码为(：)?:?|：|g; s|，.+||g}" | sed -r "s/【京东账号/My$3/;s/】.*?：/='/;s/】.*?/='/;s/$/'/;s/\(每次运行都变化,不影响\)//")
+        2)
+          codes=$(cat ${log} | grep -E "开始【京东账号|您的(好友)?助力码为" | uniq | perl -0777 -pe "{s|\*||g; s|开始||g; s|\n您的(好友)?助力码为(：)?:?|：|g; s|，.+||g}" | sed -r "s/【京东账号/My$2/;s/】.*?：/='/;s/】.*?/='/;s/$/'/;s/\(每次运行都变化,不影响\)//")
           ;;
-        4)
-          codes=$(grep -E $4 ${log} | sed -r "s/【京东账号/My$3/;s/（.*?】/='/;s/$/'/")
+        3)
+          codes=$(grep -E $3 ${log} | sed -r "s/【京东账号/My$2/;s/（.*?】/='/;s/$/'/")
           ## 添加判断，若未找到该用户互助码，则设置为空值
-          for ((user_num=1;user_num<=$1;user_num++));do
-            echo -e "${codes}" | grep -Eq "My$3${user_num}"
+          for ((user_num=1;user_num<=${UserSum};user_num++));do
+            echo -e "${codes}" | grep -Eq "My$2${user_num}"
             if [ $? -eq 1 ];then
-              codes=$(echo "${codes}" | sed -r "/My$3$(expr ${user_num} - 1)=/a\My$3${user_num}=''") 
+              codes=$(echo "${codes}" | sed -r "/My$2$(expr ${user_num} - 1)=/a\My$2${user_num}=''") 
             fi
           done
           ;;
       esac
       [[ ${codes} ]] && break
     done
+
     if [[ ${codes} ]]; then
       ## 导出为他人助力变量
       help_code=""
-      for ((user_num=1;user_num<=$1;user_num++));do
-        echo -e "${codes}" | grep -Eq "My$3${user_num}=''"
+      for ((user_num=1;user_num<=${UserSum};user_num++));do
+        echo -e "${codes}" | grep -Eq "My$2${user_num}=''"
         if [ $? -eq 1 ]; then
-          help_code=${help_code}"\${My"$3${user_num}"}@"
+          help_code=${help_code}"\${My"$2${user_num}"}@"
         fi
       done
-      ForOtherCodes=""
-      for ((user_num=1;user_num<=$1;user_num++));do
-        new_code=$(echo ${help_code} | sed "s/\${My"$3${user_num}"}@//;s/@$//")
-        ForOtherCodes=${ForOtherCodes}"ForOther"$3${user_num}"=\""${new_code}"\"\n"
-      done
-      echo -e "${codes}\n\n${ForOtherCodes}" | sed s/[[:space:]]//g
+
+      ## 生成互助规则模板
+      for_other_codes=""
+      case $HelpType in
+        0) ### 统一助力模板
+          new_code=$(echo ${help_code} | sed "s/@$//")
+          for ((user_num=1;user_num<=${UserSum};user_num++));do
+            if [ $user_num == 1 ]; then
+              for_other_codes=${for_other_codes}"ForOther"$2${user_num}"=\""${new_code}"\"\n"
+            else
+              for_other_codes=${for_other_codes}"ForOther"$2${user_num}"=\"\${ForOther"${2}1"}\"\n"
+            fi
+          done
+          ;;
+        *) ### 默认助力模板
+          for ((user_num=1;user_num<=${UserSum};user_num++));do
+            new_code=$(echo ${help_code} | sed "s/\${My"$2${user_num}"}@//;s/@$//")
+            for_other_codes=${for_other_codes}"ForOther"$2${user_num}"=\""${new_code}"\"\n"
+          done
+          ;;
+      esac
+
+      echo -e "${codes}\n\n${for_other_codes}" | sed s/[[:space:]]//g
     else
       echo ${Tips}
     fi
   else
-    echo "未运行过 jd_$2 脚本，未产生日志"
+    echo "未运行过 jd_$1 脚本，未产生日志"
   fi
 }
 
@@ -65,7 +106,7 @@ function Cat_All {
   echo -e "\n从最后一个日志中寻找互助码，仅供参考。"
   for ((i=0; i<${#Name1[*]}; i++)); do
     echo -e "\n${Name2[i]}："
-    [[ $(Cat_Scodes "${CookieNum}" "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码") == ${Tips} ]] && Cat_Scodes "${CookieNum}" "${Name1[i]}" "${Name3[i]}" || Cat_Scodes "${CookieNum}" "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码"
+    [[ $(Cat_Scodes "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码") == ${Tips} ]] && Cat_Scodes "${Name1[i]}" "${Name3[i]}" || Cat_Scodes "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码"
   done
 }
 
@@ -73,4 +114,6 @@ function Cat_All {
 LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
 LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
 [ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
+Import_Conf
+Count_UserSum
 Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|}" | tee ${LogFile}
