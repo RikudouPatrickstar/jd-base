60d1530c83d2bb0c75786cc318ee766ebea80ba3
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2021-03-14 17:48:10 +0800

    export_sharecodes.sh 一些调整

--- export_sharecodes.sh
+++ export_sharecodes.sh
@@ -8,10 +8,11 @@ FileConf=${ConfigDir}/config.sh
 Tips="从日志中未找到任何互助码"
 
 ## 所有有互助码的活动，只需要把脚本名称去掉前缀 jd_ 后列在 Name1 中，将其中文名称列在 Name2 中，对应 config.sh 中互助码后缀列在 Name3 中即可。
-## Name1、 Name2 和 Name3 中的三个名称必须一一对应。
+## Name1、Name2 和 Name3 中的三个名称必须一一对应。
 Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash sgmh cfd global)
 Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金 闪购盲盒 京喜财富岛 环球挑战赛)
 Name3=(Fruit Pet Bean DreamFactory JdFactory Joy Jdzz Jxnc BookShop Cash Sgmh Cfd Global)
+
 
 ## 导入 config.sh
 function Import_Conf {
@@ -28,6 +29,7 @@ function Import_Conf {
   fi
 }
 
+
 ## 用户数量 UserSum
 function Count_UserSum {
   for ((i=1; i<=1000; i++)); do
@@ -37,11 +39,13 @@ function Count_UserSum {
   done
 }
 
+
 ## 导出互助码的通用程序
 function Cat_Scodes {
   if [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
     cd ${LogDir}/jd_$1
-    ## 导出Cookie列表助力码变量
+    
+    ## 导出助力码变量（My）
     for log in $(ls -r); do
       case $# in
         2)
@@ -49,38 +53,39 @@ function Cat_Scodes {
           ;;
         3)
           codes=$(grep -E $3 ${log} | uniq | sed -r "s/【京东账号/My$2/;s/（.*?】/='/;s/$/'/")
-          ## 添加判断，若未找到该用户互助码，则设置为空值
-          for ((user_num=1;user_num<=${UserSum};user_num++));do
-            echo -e "${codes}" | grep -Eq "My$2${user_num}="
-            if [ $? -eq 1 ];then
-              if [ $user_num == 1 ]; then
-                codes=$(echo "${codes}" | sed -r "1i My${2}1=''")
-              else
-                codes=$(echo "${codes}" | sed -r "/My$2$(expr ${user_num} - 1)=/a\My$2${user_num}=''")
-              fi
-            fi
-          done
           ;;
       esac
-      [[ ${codes} ]] && break
+      if [[ ${codes} ]]; then
+        ## 添加判断，若未找到该用户互助码，则设置为空值
+        for ((user_num=1;user_num<=${UserSum};user_num++)); do
+          echo -e "${codes}" | grep -Eq "My$2${user_num}="
+          if [ $? -eq 1 ]; then
+            if [ $user_num == 1 ]; then
+              codes=$(echo "${codes}" | sed -r "1i My${2}1=''")
+            else
+              codes=$(echo "${codes}" | sed -r "/My$2$(expr ${user_num} - 1)=/a\My$2${user_num}=''")
+            fi
+          fi
+        done
+        break
+      fi
     done
 
+    ## 导出为他人助力变量（ForOther）
     if [[ ${codes} ]]; then
-      ## 导出为他人助力变量
       help_code=""
-      for ((user_num=1;user_num<=${UserSum};user_num++));do
+      for ((user_num=1;user_num<=${UserSum};user_num++)); do
         echo -e "${codes}" | grep -Eq "My$2${user_num}=''"
         if [ $? -eq 1 ]; then
           help_code=${help_code}"\${My"$2${user_num}"}@"
         fi
       done
-
       ## 生成互助规则模板
       for_other_codes=""
       case $HelpType in
         0) ### 统一优先级助力模板
           new_code=$(echo ${help_code} | sed "s/@$//")
-          for ((user_num=1;user_num<=${UserSum};user_num++));do
+          for ((user_num=1;user_num<=${UserSum};user_num++)); do
             if [ $user_num == 1 ]; then
               for_other_codes=${for_other_codes}"ForOther"$2${user_num}"=\""${new_code}"\"\n"
             else
@@ -89,7 +94,7 @@ function Cat_Scodes {
           done
           ;;
         1) ### 均匀助力模板
-          for ((user_num=1;user_num<=${UserSum};user_num++));do
+          for ((user_num=1;user_num<=${UserSum};user_num++)); do
             echo ${help_code} | grep "\${My"$2${user_num}"}@" > /dev/null
             if [ $? -eq 0 ]; then
               left_str=$(echo ${help_code} | sed "s/\${My$2${user_num}}@/ /g" | awk '{print $1}')
@@ -104,7 +109,7 @@ function Cat_Scodes {
           done
           ;;
         *) ### 普通优先级助力模板
-          for ((user_num=1;user_num<=${UserSum};user_num++));do
+          for ((user_num=1;user_num<=${UserSum};user_num++)); do
             new_code=$(echo ${help_code} | sed "s/\${My"$2${user_num}"}@//;s/@$//")
             for_other_codes=${for_other_codes}"ForOther"$2${user_num}"=\""${new_code}"\"\n"
           done
@@ -115,24 +120,25 @@ function Cat_Scodes {
     else
       echo ${Tips}
     fi
+
   else
     echo "未运行过 jd_$1 脚本，未产生日志"
   fi
 }
 
+
 ## 汇总
 function Cat_All {
-  echo -e "\n从最后一个日志中寻找互助码，仅供参考。"
+  echo -e "\n从最后一个日志提取互助码，受日志内容影响，仅供参考。"
   for ((i=0; i<${#Name1[*]}; i++)); do
     echo -e "\n${Name2[i]}："
     [[ $(Cat_Scodes "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码") == ${Tips} ]] && Cat_Scodes "${Name1[i]}" "${Name3[i]}" || Cat_Scodes "${Name1[i]}" "${Name3[i]}" "的${Name2[i]}好友互助码"
   done
 }
 
+
 ## 执行并写入日志
 LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
 LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
 [ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
-Import_Conf
-Count_UserSum
-Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|}" | tee ${LogFile}
+Import_Conf && Count_UserSum && Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|}" | tee ${LogFile}
